5f08ecffe517d0865c53d4ad0b4f5aa30bd2fd46
Artur Kotyra <kotyraartur@gmail.com>
2022-06-20 03:56:53 +0200

    Add files via upload

---
+++ Models/TaskModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PomoToDoro.Models;
+
+public class TaskModel
+{
+    public int TaskId { get; set; }
+    [DisplayName("Tytuł zadania:")]
+    public String Name { get; set; }
+    public bool IsDone { get; set; }
+}
